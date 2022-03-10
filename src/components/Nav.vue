c9e917ce07966eb8c34fc132dae7c2adbf854ce5
Carlos G <cgonz490@stu.southtexascollege.edu>
2022-03-10 16:31:07 -0600

    created layout

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="bg-gray-800 px-6">
-    <div class="conitainer mx-auto flex justify-center items-center text-white">
-        <router-link to="/"><h1 class="py-8 tracking-tighter text-4xl font-light text-green-600 hover:cursor-pointer">
-            <span class="font-bold">B</span>reaking<span class="font-bold">B</span>ad<span class="text-3xl">Quotes</span> 
+<div class="bg-green-900 px-6">
+    <div class="conitainer mx-auto flex justify-center items-center">
+        <router-link to="/"><h1 class="py-8 tracking-tighter text-5xl font-extralight text-gray-200 hover:cursor-pointer">
+            <span class="font-bold text-white">Br</span>eaking<span class="font-bold text-white">Ba</span>d<span class="text-3xl">Quotes</span> 
         </h1></router-link>
         
     </div>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,10 @@
 <template>
-    <h1 class="text-4xl font-semibold pt-5">Welcome To Vue Router</h1>
-  <div class="w-2/3 mx-auto pt-10 text-xl font-light">
-    <p>Vue Router is the official router for Vue.js. It deeply 
-       integrates with Vue.js core to make building Single Page
-       Applications with Vue.js a breeze. Features include:</p>
-      <ul class="pt-5 list-disc">
-        <li>Nested routes mapping</li>
-        <li>Dynamic Routing</li>
-        <li>Modular, component-based router configuration</li>
-        <li>Route params, query, wildcards</li>
-        <li>View transition effects powered by Vue.js' transition system</li>
-        <li>Fine-grained navigation control</li>
-        <li>Links with automatic active CSS classes</li>
-        <li>HTML5 history mode or hash mode</li>
-        <li>Customizable Scroll Behavior</li>
-        <li>Proper encoding for URLs</li>
-      </ul>
+    <h1 class="text-4xl font-semibold pt-5">Your BreakingBad daily motivation</h1>
+  <div class="w-2/3 mx-auto p-5 mt-10 text-xl font-light bg-slate-500">
+    <button class="bg-lime-500 rounded-md p-2 text-white font-light">Give me something</button>
+    <div class="grid grid-cols-2 gap-10 pt-8"> 
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam eum, autem corrupti alias exercitationem, dolores iste deleniti ut, culpa expedita omnis quas cupiditate asperiores provident quod iure fugit! Harum, tempora!</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam eum, autem corrupti alias exercitationem, dolores iste deleniti ut, culpa expedita omnis quas cupiditate asperiores provident quod iure fugit! Harum, tempora!</p>
+    </div>
   </div>
 </template>
